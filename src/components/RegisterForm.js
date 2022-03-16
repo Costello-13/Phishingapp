c411f02b7e32b7b64ac6de83ad7360992db9b9ea
Renke Cools <renkecools@MacBook-Pro-van-Renke.local>
2022-03-16 13:58:09 +0100

    firebase_integration

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,41 +1,50 @@
-import Wrap from "./ui/Wrap"
-import classes from "./RegisterForm.module.css"
-import { useRef } from 'react';
-function RegisterForm(){
-    const nameref = useRef();
-    const emailref = useRef();
+import Wrap from "./ui/Wrap";
+import classes from "./RegisterForm.module.css";
+import { useRef } from "react";
+import {db} from "../firebase";
+import { collection, addDoc } from "firebase/firestore"; 
 
-    function submitHandler(event){
-        event.preventDefault();
+function RegisterForm() {
+  const nameref = useRef();
+  const emailref = useRef();
 
-        const enteredName = nameref.current.value;
-        const enteredEmail = emailref.current.value;
+  function submitHandler(event) {
+    event.preventDefault();
 
-        const formData = {
-            name: enteredName,
-            email: enteredEmail
-        };
+    const enteredName = nameref.current.value;
+    const enteredEmail = emailref.current.value;
 
-        console.log(formData);
+    // const formData = {
+    //   name: enteredName,
+    //   email: enteredEmail,
+    // };
 
-    }
+    const docRef = addDoc(collection(db, "Register"), {
+      name: enteredName,
+      email: enteredEmail
+    });
+    
 
+    //console.log("hello");
+  }
 
-    return <Wrap>
-        <form className={classes.form} onSubmit={submitHandler}>
-            <div className={classes.control}>
-                <label>Full Name</label>
-                <input type="text" required id="name" ref={nameref}/>
-            </div>
-            <div className={classes.control}>
-                <label>Email</label>
-                <input type="text" required id="email" ref={emailref}/>
-            </div>
-            <div className={classes.actions}>
-                <button>Submit</button>
-            </div>
-        </form>
+  return (
+    <Wrap>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label>Full Name</label>
+          <input type="text" required id="name" ref={nameref} />
+        </div>
+        <div className={classes.control}>
+          <label>Email</label>
+          <input type="text" required id="email" ref={emailref} />
+        </div>
+        <div className={classes.actions}>
+          <button>Submit</button>
+        </div>
+      </form>
     </Wrap>
+  );
 }
 
 export default RegisterForm;
---
+++ src/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDeXMzLmtj5UzOEN9OiHYcWqJVOFeMBT60",
+
+  authDomain: "projectphish-22ab6.firebaseapp.com",
+
+  projectId: "projectphish-22ab6",
+
+  storageBucket: "projectphish-22ab6.appspot.com",
+
+  messagingSenderId: "735538299446",
+
+  appId: "1:735538299446:web:ad6d403e3e556bde360e97",
+
+  measurementId: "G-CQ64DV88HZ"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
