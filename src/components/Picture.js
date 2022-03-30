2434e6701fa82ab671ddea333b0709c63ad49d4f
Renke Cools <renkecools@MacBook-Pro-van-Renke.local>
2022-03-30 15:28:57 +0200

    gameupdate

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import pic from "./login.png";
-
+import classes from "./Picture.module.css";
 function Picture() {
-  const [coord, setCoord] = useState([]);
+  //const [coord, setCoord] = useState([]);
   const handleMouseMove = (event) => {
-    let coordinates = [event.clientX, event.clientY];
-    setCoord(coordinates);
-    console.log(coordinates);
+    alert("Correct");
   };
 
- if (coord[0] >= 410 && coord[0] <= 537 && coord[1] >= 168 && coord[1] <= 217){
-     alert("you clicked correct");
- }
-
-
-
+//  if (coord[0] >= 410 && coord[0] <= 537 && coord[1] >= 168 && coord[1] <= 217){
+//      alert("you clicked correct");
+//  }
 
   return (
-    <img src={pic} onClick={(event) => handleMouseMove(event)}/>
+    <div className={classes.parent}>
+        <img className={classes.image} src={pic}/>
+        <div className={classes.child} onClick={(event) => handleMouseMove(event)}/>
+    </div>
+    
   );
 }
 
